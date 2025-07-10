7dc93d7a2513d3a916fbda50e1b3344230ce1d77
mmmharius <mariuspapin@rcmm.fr>
2025-07-10 08:21:25 +0200

    strcmp start, not good but i have the ideas for what

--- ft_strcmp.s
+++ ft_strcmp.s
@@ -2,8 +2,29 @@ section .text
 global ft_strcmp
 
 ft_strcmp:
-    ; rdi = s1, rsi = s2
+    ; RDI = première chaîne (s1)
+    ; RSI = deuxième chaîne (s2)
 
+boucle:
+    mov al, [rdi]
+    mov bl, [rsi]
 
-fini:
-    ret ; ret de bas rax, result = rax
+    cmp al, bl
+    jne differents
+
+    cmp al, 0
+    je egaux
+
+    inc rdi
+    inc rsi
+    jmp boucle
+
+differents:
+    movzx rax, al 
+    movzx rbx, bl 
+    sub rax, rbx  
+    ret
+
+egaux:
+    mov rax, 0    
+    ret
