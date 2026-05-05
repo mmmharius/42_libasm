53e3e78ff457002eeb14e091ba92c2d1b17ddd87
mmmharius <mpapin@student.42lehavre.fr>
2026-05-05 12:22:34 +0200

    better main

--- main.c
+++ main.c
@@ -14,115 +14,87 @@
 #include <string.h>
 #include <unistd.h>
 #include <stdlib.h>
+#include <errno.h>
 
 int     ft_strcmp(const char *s1, const char *s2);
 size_t  ft_strlen(const char *s);
-char    *ft_stpcpy(char *restrict dst, const char *restrict src);
+char    *ft_strcpy(char *restrict dst, const char *restrict src);
 ssize_t ft_write(int fd, char *buf, int nbyte);
 ssize_t ft_read(int fd, void *buf, size_t count);
 char    *ft_strdup(const char *s);
 
-int     main(int argc, char **argv)
+int     main()
 {
-    //ft_strlen.s
-    // if (argc != 2)
-    //     return(printf("./libasm \"s1\"\n"), 1);
-    // else
-    // {
-    //     printf("mon strlen : %d\n", ft_strlen(argv[1]));
-    //     printf("the strlen : %ld\n", strlen(argv[1]));
-    // }
-    
-    //ft_strcpy.s
-    // if (argc != 3)
-    //     return(printf("./libasm \"s1\"\n"), 1);
-    // else
-    // {
-    //     char buffer_1[ft_strlen(argv[1])];
-    //     char buffer_2[ft_strlen(argv[1])];
+    char    *ex_str = "exemple";
+    char    *ex_str1 = "exemple1";
+    char    ex_buf[strlen(ex_str)];
+    char    ex_buff[strlen(ex_str)];
+    char    buf_ft[64];
+    char    buf_std[64];
+    char    *result_ft;
+    char    *result_std;
+    int     err;
+    int     pipefd_ft[2];
+    int     pipefd_std[2];
+    ssize_t ret;
 
-    //     printf("mon srcpy renvoie : %s\n", ft_strcpy(buffer_1, argv[1]));  
-    //     printf("the srcpy renvoie : %s\n", strcpy(buffer_2, argv[1]));
-    // }
-    
-    //ft_strcmp.s
-    // if (argc != 3)
-    //     return(printf("./libasm \"s1\" \"s2\"\n"), 1);
-    // else
-    // {
-    //     printf("mon strcmp : %d\n", ft_strcmp(argv[1], argv[2]));
-    //     printf("the strcmp : %d\n", strcmp(argv[1], argv[2]));
-    // }
-        
-    
-    //ft_write.s
-    // if (argc != 4)
-    //     return(printf("./libasm \"fd\" \"msg\" \"nb_bytes\"\n"), 1);
-    // else
-    // {
-    //     int fd = atoi(argv[1]);
-    //     int size = atoi(argv[3]);
-    //     ssize_t result_1;
-    //     ssize_t result_2;
-        
-    //     printf("---MON WRITE---\n");
-    //     result_1 = ft_write(fd, argv[2], size);
-    //     printf("renvoie: %d\n", result_1);
-        
-    //     printf("---THE WRITE---\n");
-    //     result_2 = write(fd, argv[2], size);
-    //     printf("renvoie: %ld\n", result_2);
-    // }
-    
-    //ft_read.s
-    // if (argc != 4)
-    //     return(printf("./libasm \"fd\" \"msg\" \"nb_bytes\"\n"), 1);
-    // else
-    // {
-    //     ssize_t result_1;
-    //     ssize_t result_2;
-    //     int fd = atoi(argv[1]);
-    //     int nb_bytes = atoi(argv[3]);
-    //     char buffer_1[1000] = {0};
-    //     char buffer_2[1000] = {0};
-        
-    //     printf("---MON READ---\n");
-    //     strcpy(buffer_1, argv[2]);
-    //     result_1 = nb_bytes;
-    //     printf("renvoie: %ld\n", result_1);
-    //     printf("contenu: '%.*s'\n", nb_bytes, buffer_1);
-        
-    //     printf("---THE READ---\n");
-    //     strcpy(buffer_2, argv[2]);
-    //     result_2 = nb_bytes;
-    //     printf("renvoie: %ld\n", result_2);
-    //     printf("contenu: '%.*s'\n", nb_bytes, buffer_2);
-    // }
+    printf("ft_strlen.s\n");
+    printf("my strlen : %zu\n", ft_strlen(ex_str));
+    printf("the strlen : %ld\n\n", strlen(ex_str));
 
-    //ft_strdup.s
-    if (argc != 2)
-        return(printf("./libasm \"string\"\n"), 1);
-    else
-    {
-        char *result_ft;
-        char *result_std;
-        
-        printf("---MON STRDUP---\n");
-        result_ft = ft_strdup(argv[1]);
-        printf("original: '%s' (addr: %p)\n", argv[1], argv[1]);
-        printf("ft_strdup: '%s' (addr: %p)\n", result_ft, result_ft);
-        printf("contenu: %s\n", result_ft);
-        printf("adresse : %s\n", result_ft);
-        
-        printf("---THE STRDUP---\n");
-        result_std = strdup(argv[1]);
-        printf("original: '%s' (addr: %p)\n", argv[1], argv[1]);
-        printf("strdup: '%s' (addr: %p)\n", result_std, result_std);
-        printf("contenue : %s\n", result_std);
-        printf("adresse : %s\n", result_std);
-        
-        free(result_ft);
-        free(result_std);
-    }
+    printf("ft_strcpy.s\n");
+    printf("my strcpy return : %s\n", ft_strcpy(ex_buf, ex_str));
+    printf("the strcpy return : %s\n", strcpy(ex_buff, ex_str));
+
+    printf("ft_strcmp.s\n");
+    printf("my strcmp : %d\n", ft_strcmp(ex_str, ex_str1));
+    printf("the strcmp : %d\n\n", strcmp(ex_str, ex_str1));
+
+    printf("ft_write.s\n");
+    printf("my write: %ld\n", ft_write(1, ex_str, strlen(ex_str)));
+    printf("the write: %ld\n", write(1, ex_str, strlen(ex_str)));
+    printf("ft_write.s errno test :\n");
+    errno = 0;
+    ret = ft_write(-1, ex_str, 5);
+    err = errno;
+    printf("my write (fd=-1): ret=%ld, errno=%d\n", ret, err);
+    errno = 0;
+    ret = write(-1, ex_str, 5);
+    err = errno;
+    printf("the write (fd=-1): ret=%ld, errno=%d\n\n", ret, err);
+
+    printf("ft_read.s\n");
+    memset(buf_ft, 0, 64);
+    memset(buf_std, 0, 64);
+    pipe(pipefd_ft);
+    write(pipefd_ft[1], "hello pipe\n", 11);
+    close(pipefd_ft[1]);
+    ret = ft_read(pipefd_ft[0], buf_ft, 11);
+    close(pipefd_ft[0]);
+    printf("my read: ret=%ld, content='%s'\n", ret, buf_ft);
+    pipe(pipefd_std);
+    write(pipefd_std[1], "hello pipe\n", 11);
+    close(pipefd_std[1]);
+    ret = read(pipefd_std[0], buf_std, 11);
+    close(pipefd_std[0]);
+    printf("the read: ret=%ld, content='%s'\n", ret, buf_std);
+    printf("ft_read.s errno test :\n");
+    errno = 0;
+    ret = ft_read(-1, buf_ft, 5);
+    err = errno;
+    printf("my read (fd=-1): ret=%ld, errno=%d\n", ret, err);
+    errno = 0;
+    ret = read(-1, buf_std, 5);
+    err = errno;
+    printf("the read (fd=-1): ret=%ld, errno=%d\n\n", ret, err);
+
+    printf("ft_strdup.s\n");
+    result_ft = ft_strdup(ex_str);
+    result_std = strdup(ex_str);
+    printf("my strdup: '%s' (addr: %p)\n", result_ft, (void *)result_ft);
+    printf("the strdup: '%s' (addr: %p)\n", result_std, (void *)result_std);
+    free(result_ft);
+    free(result_std);
+
     return (0);
 }
